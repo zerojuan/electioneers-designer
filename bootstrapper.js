08852fece84a3fe937b337578d3b85e10ee422cd
Julius Cebreros <me@juliuscebreros.com>
2015-04-03 21:23:22 +1300

    Initial commit

---
+++ bootstrapper.js
@@ -0,0 +1,14 @@
+/*
+#
+# Bootstrap the app from here
+#
+*/
+'use strict';
+
+angular.element(document).ready(function(){
+	//load or create settings files here
+	
+
+
+	angular.bootstrap(document, ['paDesignerApp']);
+});
---
+++ js/main/main.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('paDesignerApp')
+  .controller('MainCtrl', function($scope){    
+    $scope.message = 'Hello, my man';
+  });
