f022c650283168ead1d22d740065eb51e1b0222c
Julius Cebreros <me@juliuscebreros.com>
2015-07-11 06:36:04 +1200

    Add firstnames

---
+++ model/Dictionary.js
@@ -0,0 +1,52 @@
+'use strict';
+
+
+exports.Surname = [
+  'Santos',
+  'Reyes',
+  'Cruz',
+  'Bautista',
+  'Ocampo',
+  'Garcia',
+  'Mendoza',
+  'Torres',
+  'Tomas',
+  'Andrada',
+  'Castillo',
+  'Flores',
+  'Villanueva',
+  'Ramos',
+  'Castro',
+  'Rivera',
+  'Aquino',
+  'Navarro',
+  'Salazar',
+  'Mercado',
+  'de la Cruz',
+  'de los Reyes',
+  'del Rosario',
+  'de los Santos',
+  'de Guzman',
+  'de Castro',
+  'de la Rosa',
+  'de Asis',
+  'de Rosales'
+];
+
+exports.MaleNames = [
+  'Adam',
+  'Albert',
+  'Andrew',
+  'Bart',
+  'Bert',
+  'Bogart',
+  'Carlos',
+  'Carl',
+  'Carey',
+  'Duke',
+  'Duckey',
+  'David',
+  'Dick',
+  'Filipe',
+  'Ilia'
+];
