236acc8755084c81794ef21a46bc93b36a4faf28
Julius Cebreros <me@juliuscebreros.com>
2015-09-06 23:00:38 +1200

    Create directive for geographic view

---
+++ js/directives/Geographic/GeographicView.js
@@ -0,0 +1,17 @@
+'use strict';
+
+angular.module('paDesignerApp')
+  .directive('geographicView', function(){
+    return {
+     templateUrl: 'js/directives/Geographic/geographicview.html',
+     restrict: 'E',
+     transclude: true,
+     scope: {
+       population: '=',
+       districts: '='
+     },
+     link: function (scope) {
+      
+     }
+   };
+  });
