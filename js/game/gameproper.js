3dad4faf0ce395dc6e3eaa868fe31605bb5caa49
Julius Cebreros <me@juliuscebreros.com>
2015-09-06 22:47:03 +1200

    Remove loading for voters

--- js/game/gameproper.js
+++ js/game/gameproper.js
@@ -8,23 +8,6 @@ angular.module('paDesignerApp')
       name: 'Idiot'
     };
     //console.log('You have access to game data people!')
-    var p = LoaderService.GetVoter();
-    p.then(function(data){
-      console.log('Load Voter...', data);
-      $scope.voter = data;
-      var voters = LoaderService.GetFamilies('db2');
-      voters.then(function(data){
-        console.log('Returned data: ', data.rows[0]);
-        //load
-        $scope.voters = data.rows;
-        $scope.$apply();
-      }).catch(function(err){
-        console.log('Error: ', err);
-      });
-      $scope.$apply();
-    }).catch(function(err){
-      console.log('Error: ', err);
-    });
-
+    
 
   });
