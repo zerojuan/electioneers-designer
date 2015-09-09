a33c5e690cae05c648bc6b9a7e555825659d91b0
Julius Cebreros <me@juliuscebreros.com>
2015-09-09 23:51:47 +1200

    Render force directed graph

--- js/directives/Geographic/GeographicView.js
+++ js/directives/Geographic/GeographicView.js
@@ -13,14 +13,72 @@ angular.module('paDesignerApp')
       link: function(scope,elm) {
         var svg = d3.select(elm[0])
             .select('svg');
+        var width = 960,
+            height = 500;
+
+        var force = d3.layout.force()
+          .charge(-120)
+          .linkDistance(30)
+          .size([width, height]);
 
         var renderGeomap = function(){
-          //TODO: add rendering here
+          var nodes,
+              links = [];
+
+          console.log(scope.districts);
+          nodes = scope.districts;
+
+          //create links
+          _.forEach(nodes, function(node, i){
+            var neighbors = node.neighbors;
+            console.log(neighbors);
+            _.forEach(neighbors, function(neighbor){
+              var nid = _.findIndex(nodes, function(n){
+                return n.id === neighbor.id;
+              });
+              var link = {
+                source: i,
+                target: nid,
+                value: 1
+              };
+              links.push(link);
+            });
+          });
+
+          force
+            .nodes(nodes)
+            .links(links)
+            .start();
+
+          var link = svg.selectAll('.link')
+              .data(links)
+            .enter().append('line')
+              .attr('class', 'link')
+              .style('stroke-width', function(d){
+                return 1;
+              });
+
+          var node = svg.selectAll('.node')
+              .data(nodes)
+            .enter().append('circle')
+              .attr('class', 'node')
+              .attr('r', 5)
+              .call(force.drag);
+
+          force.on('tick', function(){
+            link.attr('x1', function(d) { return d.source.x; })
+                .attr('y1', function(d) { return d.source.y; })
+                .attr('x2', function(d) { return d.target.x; })
+                .attr('y2', function(d) { return d.target.y; });
+
+            node.attr('cx', function(d) { return d.x; })
+                .attr('cy', function(d) { return d.y; });
+          });
         };
 
-        svg.append('g');
-
-        renderGeomap();
+        scope.$watch('districts', function(){
+          renderGeomap();
+        })
       }
     };
   });
