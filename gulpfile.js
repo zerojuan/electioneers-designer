32b93f40a338a0a93d229bc599e321f9a738fea5
zerojuan <juliuscebreros@gmail.com>
2015-10-18 21:53:36 +1300

    Add gulp task

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+var watch = require('gulp-watch');
+var less = require('gulp-less');
+var concat = require('gulp-concat');
+
+gulp.task('styles', function(){
+  return gulp.src('./less/main.less')
+    .pipe(less({
+      paths: ['./less']
+    }))
+    .pipe(gulp.dest('./styles'));
+})
+
+gulp.task('default', function(){
+  gulp.start('styles');
+
+  watch( './less/*.less', function(){
+    gulp.start('styles');
+  });
+});
