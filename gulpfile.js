ddeabb011c7d7842f982049f06a268856510d880
Julius Cebreros <me@juliuscebreros.com>
2015-12-08 23:03:19 +1300

    Webpack skeleton

--- gulpfile.js
+++ gulpfile.js
@@ -3,20 +3,6 @@
 var gulp = require('gulp');
 var watch = require('gulp-watch');
 var less = require('gulp-less');
-// var concat = require('gulp-concat');
-
-gulp.task('styles', function(){
-  return gulp.src('./less/main.less')
-    .pipe(less({
-      paths: ['./less']
-    }))
-    .pipe(gulp.dest('./styles'));
-});
-
-gulp.task('default', function(){
-  gulp.start('styles');
-
-  watch( './less/*.less', function(){
-    gulp.start('styles');
-  });
-});
+var webpackDevServer = require( 'webpack-dev-server' );
+var webpackConfig = require( './webpack.config.js' );
+var webpackProductionConfig = require( './webpack.production.config.js' );
