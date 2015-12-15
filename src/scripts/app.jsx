fb9486950b061fd09d6095cc92c4ba93c6d34ad9
Julius Cebreros <me@juliuscebreros.com>
2015-12-15 23:33:26 +1300

    Use material-ui for icons

--- src/scripts/app.jsx
+++ src/scripts/app.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Link, RouteHandler } from 'react-router';
+import mui from 'material-ui';
+
+import MenuIcon from 'react-material-icons/icons/navigation/menu';
 
 require('../styles/_import.less');
 
@@ -7,10 +10,23 @@ export default React.createClass({
   displayName: 'App',
   render() {
     return (
-      <div>
-        <ul>
-          <li><Link to="/home">Home</Link></li>
-        </ul>
+      <div id="dashboard__wrapper">
+        <div className="dashboard">
+          <div className="dashboard__content">
+            <div className="dashboard__topbar container-fluid">
+              <ul className="list-inline">
+                <li>
+                  <a href="#" className="sidebar-toggle"><i><MenuIcon/></i></a>
+                </li>
+                <li className="breadcrumnb">
+                  <Link to="/home">Home</Link>
+                </li>
+              </ul>
+            </div>
+
+          </div>
+        </div>
+
         {this.props.children}
       </div>
     );
