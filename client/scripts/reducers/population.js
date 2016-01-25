ab5b533dd75be00a08222eee31ab4280b7a7e659
Julius Cebreros <me@juliuscebreros.com>
2016-01-25 22:21:36 +1300

    Add multiple families

--- client/scripts/reducers/population.js
+++ client/scripts/reducers/population.js
@@ -1,12 +1,86 @@
 
+const Surnames = [
+  'Santos',
+  'Reyes',
+  'Cruz',
+  'Bautista',
+  'Ocampo',
+  'Garcia',
+  'Mendoza',
+  'Torres',
+  'Tomas',
+  'Andrada',
+  'Castillo',
+  'Flores',
+  'Villanueva',
+  'Ramos',
+  'Castro',
+  'Rivera',
+  'Aquino',
+  'Navarro',
+  'Salazar',
+  'Mercado',
+  'de la Cruz',
+  'de los Reyes',
+  'del Rosario',
+  'de los Santos',
+  'de Guzman',
+  'de Castro',
+  'de la Rosa',
+  'de Asis',
+  'de Rosales'
+];
+
+const MaleNames = [
+  'Adam',
+  'Albert',
+  'Andrew',
+  'Bart',
+  'Bert',
+  'Bogart',
+  'Carlos',
+  'Carl',
+  'Carey',
+  'Duke',
+  'Duckey',
+  'David',
+  'Dick',
+  'Filipe',
+  'Ilia'
+];
+
+function pickRandom( arr ) {
+  return arr[
+    Math.floor( Math.random() * arr.length ) ];
+}
+
+function guid() {
+  function s4() {
+    return Math.floor( (1 + Math.random() ) * 0x10000 )
+      .toString( 16 )
+      .substring( 1 );
+  }
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+    s4() + '-' + s4() + s4() + s4();
+}
+
+function generateFamily() {
+  return {
+    _id: guid(),
+    familyName: pickRandom( Surnames ),
+    fatherName: pickRandom( MaleNames )
+  };
+}
+
 export function batchGenerateFamily( state, action ) {
-  
+  // check for size
+  var families = [];
+  for ( let i = 0; i < action.count; i++ ) {
+    families.push( generateFamily() );
+  }
+
   return [
     ...state,
-    {
-      _id: Date.now().toString(),
-      familyName: 'Something',
-      fatherName: 'Hello'
-    }
+    ...families
   ];
 }
