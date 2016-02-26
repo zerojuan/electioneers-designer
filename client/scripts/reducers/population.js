ffc5482d4fc077173fd815085fdca34280ef4320
Julius Cebreros <me@juliuscebreros.com>
2016-02-26 22:46:42 +1300

    Refactor guid

--- client/scripts/reducers/population.js
+++ client/scripts/reducers/population.js
@@ -1,3 +1,4 @@
+import { guid } from './utils';
 
 const Surnames = [
   'Santos',
@@ -52,16 +53,6 @@ const MaleNames = [
 function pickRandom( arr ) {
   return arr[
     Math.floor( Math.random() * arr.length ) ];
-}
-
-function guid() {
-  function s4() {
-    return Math.floor( (1 + Math.random() ) * 0x10000 )
-      .toString( 16 )
-      .substring( 1 );
-  }
-  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
-    s4() + '-' + s4() + s4() + s4();
 }
 
 function generateFamily() {
