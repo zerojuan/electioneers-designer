441edd299f6fbe3e50c6c5a538498b3c8a5ec2e6
Julius Cebreros <me@juliuscebreros.com>
2016-03-25 23:13:37 +1300

    Allow phaser stuff in build workflow

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,6 +1,11 @@
 var path = require( 'path' );
 var webpack = require( 'webpack' );
 
+var phaserModule = path.join( __dirname, '/node_modules/phaser/' );
+var phaser = path.join( phaserModule, 'build/custom/phaser-split.js' ),
+  pixi = path.join( phaserModule, 'build/custom/pixi.js' ),
+  p2 = path.join( phaserModule, 'build/custom/p2.js' );
+
 module.exports = {
   devTool: 'source-map',
   entry: [
@@ -25,6 +30,11 @@ module.exports = {
   ],
 
   resolve: {
+    alias: {
+        'phaser': phaser,
+        'pixi.js': pixi,
+        'p2': p2
+    },
     extensions: [ '', '.js', '.jsx', '.coffee', '.less', '.ttf', '.eot', '.woff' ],
     moduleDirectories: [
       'node_modules',
@@ -48,6 +58,10 @@ module.exports = {
       }
     ],
     loaders: [
+      {
+        test: /(pixi.js|p2)/,
+        loader: 'script'
+      },
       {
         test: /\.css$/,
         loaders: [ 'style', 'css' ]
