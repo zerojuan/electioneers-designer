d83b8ed1120388ea5c099e1782d53df8bd454557
Julius Cebreros <me@juliuscebreros.com>
2016-04-05 22:42:18 +1200

    Remove configurability in save location

--- server/settings.js
+++ server/settings.js
@@ -1,31 +1,10 @@
 'use strict';
 
-var fs = require( 'fs' );
 var path = require( 'path' );
-
-// load settings file
-var loadSettings = function() {
-  return JSON.parse(
-    fs.readFileSync( path.join( __dirname, '/data/settings.json' ),
-      { encoding: 'utf8' })
-  );
-};
-
 
 var home = process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE;
 var defaultLoadFolder = home + '/PartyAnimalsData';
 
 exports.getWorkingDirectory = function() {
-  var settings = loadSettings();
-  if ( settings.workingFolder === 'default' ) {
-    return defaultLoadFolder;
-  } else {
-    return settings.workingFolder;
-  }
-};
-
-exports.setWorkingDirectory = function( path ) {
-  var settings = loadSettings();
-  settings.workingFolder = path;
-  fs.writeFileAsync( 'data/settings.json', JSON.stringify( settings, null, '\t' ) );
+  return defaultLoadFolder;
 };
