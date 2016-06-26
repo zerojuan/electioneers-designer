57d1cac189628c75b866990d9d83860a1bc080d4
Jean Claudette Ambait <jeanclaudetteambait@gmail.com>
2016-06-26 22:15:28 +1200

    Remove todo

--- client/scripts/actions/district.js
+++ client/scripts/actions/district.js
@@ -37,7 +37,6 @@ export function changeBackground( bgId ) {
 }
 
 export function deleteDistrict( district ) {
-  // TODO: Check if the district should be deleted
   return {
     type: DELETE_DISTRICT,
     district: district
